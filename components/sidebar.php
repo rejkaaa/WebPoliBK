64f1109e5aaec62ea976a785bcb1ac6c60cbbe39
rejkaaaa <rejkacandra231@gmail.com>
2025-01-04 11:57:09 +0700

    update profile dokter

--- components/sidebar.php
+++ components/sidebar.php
@@ -39,80 +39,87 @@
                     <?php
                     if ($_SESSION['akses'] == "admin") {
                     ?>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="dashboard_admin.php" class="nav-link">
-                                <i class="fas fa-solid fas fa-th nav-icon"></i>
-                                <p>Dashboard <span class="right badge badge-danger">Admin</span></p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="dokter.php" class="nav-link">
-                                <i class="fas fa-solid fa-user-nurse nav-icon"></i>
-                                <p>Dokter <span class="right badge badge-danger">Admin</span></p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="poli.php" class="nav-link">
-                                <i class="fas fa-solid fa-hospital nav-icon"></i>
-                                <p>Poli <span class="right badge badge-danger">Admin</span></p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="obat.php" class="nav-link">
-                                <i class="fas fa-solid fa-tablets nav-icon"></i>
-                                <p>Obat <span class="right badge badge-danger">Admin</span></p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pasien.php" class="nav-link">
-                                <i class="fas fa-solid fa-user nav-icon"></i>
-                                <p>Pasien <span class="right badge badge-danger">Admin</span></p>
-                            </a>
-                        </li>
-                    </ul>
-                    <?php } else if($_SESSION['akses']=="dokter"){ ?>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="dashboard_dokter.php" class="nav-link">
-                                <i class="fas fa-solid fas fa-th nav-icon"></i>
-                                <p>Dashboard <span class="right badge badge-success">Dokter</span></p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="jadwalPeriksa.php" class="nav-link">
-                                <i class="fas fa-solid fa-hospital-user nav-icon"></i>
-                                <p>Jadwal Periksa <span class="right badge badge-success">Dokter</span></p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="periksaPasien.php" class="nav-link">
-                                <i class="fas fa-solid fa-stethoscope nav-icon"></i>
-                                <p>Memeriksa Pasien <span class="right badge badge-success">Dokter</span></p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="riwayatPasien.php" class="nav-link">
-                                <i class="fas fa-solid fa-book-medical nav-icon"></i>
-                                <p>Riwayat Pasien <span class="right badge badge-success">Dokter</span></p>
-                            </a>
-                        </li>
-                    </ul>
-                    <?php } else if($_SESSION['akses'] == "pasien"){?>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="dashboard_pasien.php" class="nav-link">
-                                <i class="fas fa-solid fa-hospital-user nav-icon"></i>
-                                <p>Dashboard <span class="right badge badge-info">Pasien</span></p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="daftarPoliklinik.php" class="nav-link">
-                                <i class="fas fa-solid fa-stethoscope nav-icon"></i>
-                                <p>Daftar Poli <span class="right badge badge-info">Pasien</span></p>
-                            </a>
-                        </li>
-                    </ul>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="dashboard_admin.php" class="nav-link">
+                                    <i class="fas fa-solid fas fa-th nav-icon"></i>
+                                    <p>Dashboard <span class="right badge badge-danger">Admin</span></p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="dokter.php" class="nav-link">
+                                    <i class="fas fa-solid fa-user-nurse nav-icon"></i>
+                                    <p>Dokter <span class="right badge badge-danger">Admin</span></p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="poli.php" class="nav-link">
+                                    <i class="fas fa-solid fa-hospital nav-icon"></i>
+                                    <p>Poli <span class="right badge badge-danger">Admin</span></p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="obat.php" class="nav-link">
+                                    <i class="fas fa-solid fa-tablets nav-icon"></i>
+                                    <p>Obat <span class="right badge badge-danger">Admin</span></p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="pasien.php" class="nav-link">
+                                    <i class="fas fa-solid fa-user nav-icon"></i>
+                                    <p>Pasien <span class="right badge badge-danger">Admin</span></p>
+                                </a>
+                            </li>
+                        </ul>
+                    <?php } else if ($_SESSION['akses'] == "dokter") { ?>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="dashboard_dokter.php" class="nav-link">
+                                    <i class="fas fa-solid fas fa-th nav-icon"></i>
+                                    <p>Dashboard <span class="right badge badge-success">Dokter</span></p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="jadwalPeriksa.php" class="nav-link">
+                                    <i class="fas fa-solid fa-hospital-user nav-icon"></i>
+                                    <p>Jadwal Periksa <span class="right badge badge-success">Dokter</span></p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="periksaPasien.php" class="nav-link">
+                                    <i class="fas fa-solid fa-stethoscope nav-icon"></i>
+                                    <p>Memeriksa Pasien <span class="right badge badge-success">Dokter</span></p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="riwayatPasien.php" class="nav-link">
+                                    <i class="fas fa-solid fa-book-medical nav-icon"></i>
+                                    <p>Riwayat Pasien <span class="right badge badge-success">Dokter</span></p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="profile.php" class="nav-link">
+                                    <i class="nav-icon fas fa-user"></i>
+                                    <p>Update Profile <span class="right badge badge-success">Dokter</span></p>
+                                </a>
+                            </li>
+
+                        </ul>
+                    <?php } else if ($_SESSION['akses'] == "pasien") { ?>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="dashboard_pasien.php" class="nav-link">
+                                    <i class="fas fa-solid fa-hospital-user nav-icon"></i>
+                                    <p>Dashboard <span class="right badge badge-info">Pasien</span></p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="daftarPoliklinik.php" class="nav-link">
+                                    <i class="fas fa-solid fa-stethoscope nav-icon"></i>
+                                    <p>Daftar Poli <span class="right badge badge-info">Pasien</span></p>
+                                </a>
+                            </li>
+                        </ul>
                     <?php } ?>
                 </li>
                 <li class="nav-item">
